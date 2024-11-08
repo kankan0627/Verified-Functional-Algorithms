10b4e9c4fe4ce1a746f6c5d083d7ef12adcb28b3
Kankan Wang <wangkankan0627@outlook.com>
2024-11-08 18:28:08 +0800

    Update Merge.v

--- Merge.v
+++ Merge.v
@@ -73,5 +73,91 @@ Proof.
     apply IHl'. auto.
 Qed.
 
+Fixpoint merge l1 l2 {struct l1} :=
+  let fix merge_aux l2 :=
+  match l1, l2 with
+  | [], _ => l2
+  | _, [] => l1
+  | a1::l1', a2::l2' =>
+      if a1 <=? a2 then a1 :: merge l1' l2 else a2 :: merge_aux l2'
+  end
+  in merge_aux l2.
+ 
+Lemma merge2 : forall (x1 x2:nat) r1 r2,
+    x1 <= x2 ->
+    merge (x1::r1) (x2::r2) =
+    x1::merge r1 (x2::r2).
+Proof.
+  intros. simpl. destruct (x1 <=? x2) eqn:Heq. 
+  - auto.
+  - apply Nat.leb_nle in Heq. 
+    apply Heq in H. inversion H. 
+Qed.
 
+Lemma merge_nil_l : forall l, merge [] l = l.
+Proof.
+  intros.  simpl. induction l; auto.
+Qed.
+
+Function mergesort (l: list nat) {measure length l} : list nat :=
+  match l with
+  | [] => []
+  | [x] => [x]
+  | _ => let (l1,l2) := split l in
+         merge (mergesort l1) (mergesort l2)
+  end.
+Proof.
+  - intros. simpl in teq1. destruct (split l1) as [lx ly] eqn:E;
+    inversion teq1; apply split_len in E; simpl; destruct E; lia.
+  - intros. simpl in teq1. destruct (split l1) as [lx ly] eqn:E.
+     inversion teq1. simpl. apply split_len in E. destruct E. lia. 
+Defined. 
+
+Check mergesort_equation.
+
+Lemma sorted_merge1 : forall x x1 l1 x2 l2,
+    x <= x1 -> x <= x2 ->
+    sorted (merge (x1::l1) (x2::l2)) ->
+    sorted (x :: merge (x1::l1) (x2::l2)).
+Proof.
+  intros. simpl in *. destruct (x1 <=? x2) eqn:Heq.
+  - apply sorted_cons. 
+    + apply H. 
+    + apply H1. 
+  - apply sorted_cons. 
+    + apply H0. 
+    +  apply H1.
+Qed.
+      
+
+Lemma sorted_merge : forall l1, sorted l1 ->
+                     forall l2, sorted l2 ->
+                     sorted (merge l1 l2).
+Proof. 
+  induction l1. 
+  - intros H. induction l2.
+    + intros. simpl. apply H. 
+    + intros. simpl. apply H0. 
+  - intros H. induction l2.
+    + intros.  simpl. apply H. 
+    + intros.  destruct (a <=? a0) eqn:Heq.
+      * simpl. rewrite Heq. induction l1.
+        -- simpl. apply sorted_cons. 
+           ++ apply Nat.leb_le in Heq. apply Heq.
+           ++ apply H0.
+        --  inversion H. apply Nat.leb_le in Heq.  apply sorted_merge1.
+           ++ apply H3.
+           ++ apply Heq.
+           ++  apply IHl1. 
+              ** apply H5.
+              ** apply H0.
+       * simpl. rewrite Heq. induction l2.
+         -- apply sorted_cons. 
+            ++ apply leb_iff_conv in Heq. lia. 
+            ++ apply H. 
+         -- destruct (a <=? a1) eqn:Hen. 
+            ++ apply sorted_cons.
+               ** apply leb_iff_conv in Heq. lia. 
+               ** inversion H0.  Search "<=?". 
   
+
